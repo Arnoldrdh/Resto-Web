94a7583f1a474e71c53eeb208785f63863a5f259
vincnx <vincent.celcio@gmail.com>
2025-06-20 16:36:59 +0700

    feat: change readme

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="flex h-full w-full flex-col gap-4">
     <div class="container h-[calc(100dvh-4rem)] w-screen p-8 pt-0">
-        <x-hero.hero title="NKFoods" description="Lorem ipsum abc" image="{{asset('image.webp')}}" />
+        <x-hero.hero title="NKFoods" description="Berisi makanan makanan sehat dan bergizi" image="{{asset('image.webp')}}" />
     </div>
 
     <div class="container flex flex-col gap-4 px-8 xl:flex-row">
@@ -13,7 +13,7 @@
             </div>
             <div class="bg-accent-foreground min-w-92 flex flex-1 flex-col justify-center gap-4 p-8 md:justify-start">
                 <h1 class="font-outline-3 text-accent text-justify text-6xl">Menu</h1>
-                <p class="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid excepturi ad rerum incidunt magni accusantium laborum sint quos dignissimos corporis.</p>
+                <p class="mt-4">Berisi menu makanan bergizi.</p>
                 <a href="{{route('menu')}}">
                     <button class="bg-accent hover:bg-accent/80 ms-auto w-fit border-4 md:ms-0">View Menu</button>
                 </a>
@@ -26,7 +26,7 @@
             </div>
             <div class="bg-primary-foreground min-w-92 flex flex-1 flex-col justify-center gap-4 p-8 md:justify-start">
                 <h1 class="font-outline-3 text-accent text-justify text-6xl">Reservation</h1>
-                <p class="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid excepturi ad rerum incidunt magni accusantium laborum sint quos dignissimos corporis.</p>
+                <p class="mt-4">Reservasi sekarang untuk dapatkan penawaran terbaik.</p>
                 <a href="{{route('reservation')}}">
                     <button class="bg-accent hover:bg-accent/80 ms-auto w-fit border-4 md:ms-0">Reserve Now</button>
                 </a>
